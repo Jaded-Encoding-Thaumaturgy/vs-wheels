e55056fcd9e1399d873c1ab4387702c49c99ebeb
Vardë <ichunjo.le.terrible@gmail.com>
2026-05-03 20:56:29 +0200

    dfttest2: restructure package for better distribution

---
+++ dfttest2/dfttest2/dfttest2/__init__.py
@@ -0,0 +1,17 @@
+from ._dfttest2 import (  # type: ignore
+    FREQ,
+    SIGMA,
+    Backend,
+    DFTTest,
+    DFTTest2,
+    flatten,
+    get_location,
+    get_window,
+    get_window_value,
+    init_backend,
+    normalize,
+    select_backend,
+    to_func,
+)
+
+__all__ = ["Backend", "DFTTest", "DFTTest2"]
