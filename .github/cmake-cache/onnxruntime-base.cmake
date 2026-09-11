bed31456524088b69d36b2e52b59b556ce34b6f1
Vardë <ichunjo.le.terrible@gmail.com>
2026-09-12 01:07:51 +0200

    re-enable float8, sparse tensors, and optional types

--- .github/cmake-cache/onnxruntime-base.cmake
+++ .github/cmake-cache/onnxruntime-base.cmake
@@ -9,6 +9,3 @@ set(onnxruntime_ENABLE_LTO ON CACHE BOOL "")
 
 set(onnxruntime_DISABLE_ML_OPS ON CACHE BOOL "")
 set(onnxruntime_DISABLE_GENERATION_OPS ON CACHE BOOL "")
-set(onnxruntime_DISABLE_FLOAT8_TYPES ON CACHE BOOL "")
-set(onnxruntime_DISABLE_SPARSE_TENSORS ON CACHE BOOL "")
-set(onnxruntime_DISABLE_OPTIONAL_TYPE ON CACHE BOOL "")
